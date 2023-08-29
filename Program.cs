abf1a46f2ce42144c720223da6e3bcb14382edf5
Dmitry <143112331+DmitryBondarev98@users.noreply.github.com>
2023-08-29 21:59:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            bool isAlive;
+            int coins;
+            float playerDamage;
+            long quantityOfMoney;
+            byte age;
+            char selectedLetter;
+            ushort bonus;
+            double jump;
+            uint playerHealth;
+        }
+    }
+}
